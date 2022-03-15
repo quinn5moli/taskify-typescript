86ad58b67fe107a290e8320fa864a053f7b106eb
quinn5moli <quinn.moli@gmail.com>
2022-03-14 21:03:18 -0700

    input UI created

---
+++ taskify/src/components/InputField.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './styles.css'
+
+const InputField = () => {
+  return (
+  <form className= "input">
+      <input type="input" placeholder= "Enter a task" className="input-box" />
+      <button className="input-submit" type="submit">Go</button>
+  </form>
+  );
+};
+
+export default InputField
