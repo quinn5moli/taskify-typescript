13c366e7d54cb178a2ac0e5b765cc9d9c8623051
quinn5moli <quinn.moli@gmail.com>
2022-03-14 21:13:30 -0700

    props type assinged to toDo state in Input/App

--- taskify/src/App.tsx
+++ taskify/src/App.tsx
@@ -1,12 +1,16 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import InputField from './components/InputField';
 
 const App: React.FC = () => {
+
+  const [toDo, setToDo] = useState<string>("")
+
+
   return (
     <div className="App">
       <span className="heading">Taskify</span>
-      <InputField />
+      <InputField toDo={toDo} setToDo={setToDo} />
     </div>
   );
 }
--- taskify/src/components/InputField.tsx
+++ taskify/src/components/InputField.tsx
@@ -1,10 +1,21 @@
 import React from 'react'
 import './styles.css'
 
-const InputField = () => {
+interface Props{
+    toDo: string;
+    setToDo: React.Dispatch<React.SetStateAction<string>>;
+}
+
+const InputField = ({ toDo, setToDo }: Props) => {
   return (
   <form className= "input">
-      <input type="input" placeholder= "Enter a task" className="input-box" />
+      <input type="input" 
+        value={toDo}
+        onChange={
+            (e) => setToDo(e.target.value)
+        }
+        placeholder= "Enter a task" 
+        className="input-box" />
       <button className="input-submit" type="submit">Go</button>
   </form>
   );
